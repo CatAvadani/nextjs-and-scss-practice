4fd0dabac0be9d4bf22a3c031e362d9382782c77
CatAvadani <catalinaavadani@gmail.com>
2024-11-25 16:31:29 +0100

    Add form handling and styling with SCSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,58 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
 import './page.scss';
 
+interface FormData {
+  id: number;
+  title: string;
+  description: string;
+}
+
 export default function Home() {
+  const [formData, setFromData] = useState<FormData>({
+    id: Date.now(),
+    title: '',
+    description: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFromData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newFromData = {
+      title: '',
+      description: '',
+    };
+    setFromData((prev) => ({ ...prev, ...newFromData }));
+    console.log(formData);
+  };
+
   return (
     <div className='main-container'>
-      <div className='form-container'>
+      <form onSubmit={handleSubmit} className='form-container'>
         <h1>Hello, Next.js!</h1>
-        <input type='text' placeholder='Enter title' />
-        <input type='text' placeholder='Enter description' />
+        <input
+          name='title'
+          value={formData.title}
+          onChange={handleChange}
+          type='text'
+          placeholder='Enter title'
+        />
+        <input
+          name='description'
+          value={formData.description}
+          onChange={handleChange}
+          type='text'
+          placeholder='Enter description'
+        />
         <button className='submit-btn'>Submit</button>
-      </div>
+      </form>
     </div>
   );
 }
